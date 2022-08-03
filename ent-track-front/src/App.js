93ab951626d65c985ffd199175999e4c2a578080
Chris O'Brien <you@example.comchristopherwobrien03@gmail.com>
2022-08-03 18:33:32 -0500

    filled app.js file

--- ent-track-front/src/App.js
+++ ent-track-front/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
+import Home from './components/Home';
+import Animelog from './components/Anime/Anime';
+import Bookslog from './components/Books/Books';
+import Movielog from './components/Movies/Movies';
+import Mangalog from './components/Manga/Manga';
+import NewAnimeForm from './components/Anime/NewAnime';
+import NewBookForm from './components/Books/NewBook';
+import NewMangaForm from './components/Manga/NewManga';
+import NewMovieForm from './components/Movies/NewMovie';
+import PrimaryNavbar from './components/Navbar';
+import HandleScroll from './components/ScrollUp';
+import Button from "@mui/material/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  return (
+    <div>
+      <BrowserRouter>
+        <main>
+            <section className='App'>
+              <div className='App-header'>
+                <PrimaryNavbar /> 
+              </div>
+                <div>
+                      <Routes>
+
+                        <Route path="/" element={<Home />}/>
+                        <Route path="/Anime" element={<Animelog />}/>
+                        <Route path="/Books" element={<Bookslog />}/>
+                        <Route path="/Manga" element={<Mangalog />}/>
+                        <Route path="/Movies" element={<Movielog />}/>
+                        <Route path="/Anime/New" element={<NewAnimeForm />}/>
+                        <Route path="/Manga/New" element={<NewMangaForm />}/>
+                        <Route path="/Books/New" element={<NewBookForm />}/>
+                        <Route path="/Movies/New" element={<NewMovieForm />}/>
+
+                      </Routes>  
+                </div>
+            </section>
+            <div>
+            <Button id='Scroll-up' variant="contained" onClick={HandleScroll}>⬆</Button>
+            </div>
+        </main>
+      </BrowserRouter>
+    </div>
+    );
+  }
+  export default App;
